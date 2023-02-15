27d1fc2afeda7380fe0cbba4f572a3e34fb5f4d9
Chris Wood <wchristopherwood912@gmail.com>
2023-02-14 16:05:39 -0800

    added authentication

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
+from grocery_app.models import GroceryItem, GroceryStore, User
+from flask_login import login_user, logout_user, login_required, current_user
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, SignUpForm, LoginForm, AddToCartForm
 
 # Import app and db from events_app package so that we can run app
-from grocery_app.extensions import app, db
+from grocery_app.extensions import app, db, bcrypt
 
 main = Blueprint("main", __name__)
+auth = Blueprint("auth", __name__)
 
 ##########################################
 #           Routes                       #
@@ -15,7 +17,9 @@ main = Blueprint("main", __name__)
 @main.route('/')
 def homepage():
     all_stores = GroceryStore.query.all()
-    return render_template('home.html', all_stores=all_stores)
+    logged_in = current_user.is_authenticated
+    print(logged_in)
+    return render_template('home.html', all_stores=all_stores, logged_in=logged_in)
 
 @main.route('/new_store', methods=['GET', 'POST'])
 def new_store():
@@ -36,10 +40,13 @@ def new_store():
         flash("store added")
         return redirect(url_for('main.store_detail', store_id = new_store.id))
 
+    
+
     # TODO: Send the form to the template and use it to render the form fields
     return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
+@login_required
 def new_item():
     # TODO: Create a GroceryItemForm
     form = GroceryItemForm()
@@ -113,9 +120,71 @@ def item_detail(item_id):
 
         flash(f"item({item.name}) updated")
 
-        return redirect(url_for('main.item_detail', item_id = item.id))
+        return redirect(url_for('main.item_detail', item = item))
 
     # TODO: Send the form to the template and use it to render the form fields
     item = GroceryItem.query.get(item_id)
     return render_template('item_detail.html', item=item, form=form)
 
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print('in signup')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        print('created')
+        return redirect(url_for('auth.login'))
+    print(form.errors)
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
+
+@main.route('/add_to_shopping_list/<item_id>', methods=['GET', 'POST'])
+@login_required
+def add_to_shopping_list(item_id):
+    shopping_list = current_user.shopping_list_items
+
+
+    item = GroceryItem.query.get(item_id)
+    current_user.shopping_list_items.append(item)
+
+    db.session.commit()
+
+    flash("item added to cart")
+
+    return redirect(url_for('main.view_cart', user_id=current_user.id))
+
+
+@main.route('/view_cart', methods=['GET', 'POST'])
+@login_required
+def view_cart():
+    user = current_user
+
+    # TODO send users grocery list to html page for editing/viewing
+    items = db.session.query(User).filter_by(id=user.id).first().shopping_list_items
+    # user_
+    # test = User.query.filter_by(id = user.id).join(GroceryItem).filter(GroceryItem.id == User.)            
+    
+    return render_template('view_cart.html', items=items)
